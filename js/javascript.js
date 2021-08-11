9bf488de45ecefaf9dfbf9fe4a725aecf134de32
lazio@wataidesign.com <lazio@wataidesign.com>
2021-08-11 17:27:17 +0800

    110-0811-3

--- js/javascript.js
+++ js/javascript.js
@@ -843,9 +843,11 @@ $(function(){
 $(function(){
     $('.threeCircle button:nth-of-type(1)').click(function(){
         $('.indexStyle').attr('href','./css/style.css');
+        $('.elseStyle').attr('href','../../css/style.css');
     })
     $('.threeCircle button:nth-of-type(3)').click(function(){
         $('.indexStyle').attr('href','./css_2/style.css');
+        $('.elseStyle').attr('href','../../css_2/style.css');
     })
 })
 
